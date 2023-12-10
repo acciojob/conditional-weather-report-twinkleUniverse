3ce975f8dfc89e11b58f7be4f2849fe0f0b6602e
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2023-12-10 09:15:35 +0000

    solution-1

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,21 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const data= { temperature: 25, conditions: "Sunny" };
+  const treshold= 20 ;
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       {
+        data.temperature<treshold?
+        <div style={{color:'blue'}}>
+          <h1>Temperature: {data.temperature}</h1>
+          <h1>conditions: {data.conditions}</h1>
+        </div> : <div style={{color:'red'}}>
+          <h1>Temperature:{data.temperature}</h1>
+          <h1>Conditions:{data.conditions}</h1>
+        </div>
+       }
     </div>
   )
 }
